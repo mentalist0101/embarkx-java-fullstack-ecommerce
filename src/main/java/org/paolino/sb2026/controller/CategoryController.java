303849feee34bd5a325478a02b9d1a0657c6234b
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-01-02 00:45:53 +0100

    Added deleting Category feature

--- src/main/java/org/paolino/sb2026/controller/CategoryController.java
+++ src/main/java/org/paolino/sb2026/controller/CategoryController.java
@@ -3,10 +3,7 @@ package org.paolino.sb2026.controller;
 import org.paolino.sb2026.model.Category;
 import org.paolino.sb2026.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId){
+        return categoryService.deleteCategory(categoryId);
     }
 }
