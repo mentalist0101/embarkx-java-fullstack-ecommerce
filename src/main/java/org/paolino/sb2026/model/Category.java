7dffc3be1ff0bb0bea256f183c88b5d0d19bdaba
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-01-09 00:22:09 +0100

    Added @Size annotation in Category class

--- src/main/java/org/paolino/sb2026/model/Category.java
+++ src/main/java/org/paolino/sb2026/model/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,6 @@ import lombok.NoArgsConstructor;
 public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
-    @NotBlank
+    @NotBlank @Size(min = 5, message = "Category name must contain at least 5 characters")
     private String categoryName;
 }
