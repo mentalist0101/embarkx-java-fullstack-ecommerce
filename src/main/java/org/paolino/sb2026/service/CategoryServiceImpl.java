e8dfe159d358f5c9904d16bba9e4cc5c102de910
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-01-10 01:13:43 +0100

    Implemented APIException in getAllCategories() in CategoryServiceImpl

--- src/main/java/org/paolino/sb2026/service/CategoryServiceImpl.java
+++ src/main/java/org/paolino/sb2026/service/CategoryServiceImpl.java
@@ -17,7 +17,10 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty())
+            throw new APIException("No category created till now.");
+        return categories;
     }
 
     @Override
