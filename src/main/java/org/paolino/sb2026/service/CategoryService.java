3250036f9ab2a6d771c109fbf3fbf6f1e747a817
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-01-11 22:49:44 +0100

    Implemented sorting

--- src/main/java/org/paolino/sb2026/service/CategoryService.java
+++ src/main/java/org/paolino/sb2026/service/CategoryService.java
@@ -4,7 +4,7 @@ import org.paolino.sb2026.payload.CategoryDTO;
 import org.paolino.sb2026.payload.CategoryResponse;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
     CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
     CategoryDTO deleteCategory(Long categoryId);
