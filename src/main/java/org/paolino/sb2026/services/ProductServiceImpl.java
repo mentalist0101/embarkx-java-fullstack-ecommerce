f4a83e27d5a532fb31cac511c13707f49b87cc89
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-01-23 01:37:22 +0100

    Added getting Products by keyword feature

--- src/main/java/org/paolino/sb2026/services/ProductServiceImpl.java
+++ src/main/java/org/paolino/sb2026/services/ProductServiceImpl.java
@@ -56,4 +56,13 @@ public class ProductServiceImpl implements ProductService {
                 .toList();
         return new ProductResponse(productDTOS);
     }
+
+    @Override
+    public ProductResponse searchProductByKeyword(String keyword) {
+        List<Product> products = productRepository.findByProductNameLikeIgnoreCase('%' + keyword + '%');
+        List<ProductDTO> productDTOS = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+        return new ProductResponse(productDTOS);
+    }
 }
