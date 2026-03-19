58a743d1eca836c23e3ef5d80a0a4c10447ac5ba
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-03-19 23:12:09 +0100

    Implemented relationships in classes User and Address.

--- src/main/java/org/paolino/sb2026/models/Address.java
+++ src/main/java/org/paolino/sb2026/models/Address.java
@@ -7,6 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 @Table(name = "addresses")
@@ -41,4 +44,7 @@ public class Address {
         this.country = country;
         this.pincode = pincode;
     }
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
 }
