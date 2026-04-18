7c21534b0967cf0d36b6e172757fed4c95484582
mentalist0101 <45788357+mentalist0101@users.noreply.github.com>
2026-04-18 23:36:35 +0200

    Added fetching users Cart feature

--- src/main/java/org/paolino/sb2026/controllers/CartController.java
+++ src/main/java/org/paolino/sb2026/controllers/CartController.java
@@ -1,7 +1,10 @@
 package org.paolino.sb2026.controllers;
 
+import org.paolino.sb2026.model.Cart;
 import org.paolino.sb2026.payload.CartDTO;
+import org.paolino.sb2026.repositories.CartRepository;
 import org.paolino.sb2026.services.CartService;
+import org.paolino.sb2026.util.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,12 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    @Autowired
+    private AuthUtil authUtil;
+
+    @Autowired
+    private CartRepository cartRepository;
+
     @PostMapping("/carts/products/{productId}/quantity/{quantity}")
     public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId, @PathVariable Integer quantity) {
         CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
@@ -26,5 +35,14 @@ public class CartController {
     public ResponseEntity<List<CartDTO>> getCarts() {
         List<CartDTO> cartDTOs = cartService.getAllCarts();
         return new ResponseEntity<List<CartDTO>>(cartDTOs, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/carts/users/cart")
+    public ResponseEntity<CartDTO> getCartById(){
+        String emailId = authUtil.loggedInEmail();
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        Long cartId = cart.getCartId();
+        CartDTO cartDTO = cartService.getCart(emailId, cartId);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.OK);
     }
 }
